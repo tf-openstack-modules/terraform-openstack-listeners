4ba16d048b8ce5f20a994a77ac3549276679efce
Jeremy Chauvin <jeremychauvin10@gmail.com>
2020-11-07 14:57:44 +0100

    refactor(var): use variable in resource

--- 010-listeners.tf
+++ 010-listeners.tf
@@ -1,9 +1,17 @@
 # Create listeners
 resource "openstack_lb_listener_v2" "listener" {
   name            = var.name
+  description     = var.description
   protocol        = var.protocol
   protocol_port   = var.port
   loadbalancer_id = var.lb_id
+  timeout_client_data = var.timeout_client_data
+  timeout_tcp_inspect = var.timeout_tcp_inspect
+  timeout_member_connect = var.timeout_member_connect
+  timeout_member_data = var.timeout_member_data
+  connection_limit = var.connection_limit
+  admin_state_up = var.admin_state_up
+  insert_headers = var.insert_headers
 }
 
 module "pools" {
